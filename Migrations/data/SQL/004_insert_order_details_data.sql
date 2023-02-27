0ef203e91a188fea7209d9a7d91ea0ce99f097b9
RutaPaul <rutapaulaviciute14@gmail.com>
2023-02-27 20:56:41 +0200

    Added insert scripts and default csv data files

---
+++ Migrations/data/SQL/004_insert_order_details_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO egzaminas.order_details (OrderID,ProductID,UnitPrice,Quantity,Discount) VALUES
+	 (1,3,5.99,4,25),
+	 (2,4,4.52,2,10),
+	 (3,1,2.19,1,0),
+	 (4,5,4.99,20,15),
+	 (5,2,6.99,1,0);
